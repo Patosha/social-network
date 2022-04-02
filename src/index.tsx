da39963560e2d5da5e21c8f8328aadc2e1b8e1c5
Patosha <Patosha68@gmail.com>
2022-04-02 14:33:33 +0300

    feat: add state.ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,65 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-export type MessageDataPropsType = {
-    id: number
-    avatar: string
-    name: string
-    time: string
-    text: string
-}
 
-export type PostsDatePropsType = {
-    id: number
-    name: string
-    avatar: string
-    date: string
-    text: string
-    likesCount: number
-}
-
-let postsDate: Array<PostsDatePropsType> = [
-    {
-        id: 1,
-        name: 'Klark Kent',
-        avatar: 'https://shapka-youtube.ru/wp-content/uploads/2021/01/man-ava5.jpg',
-        date: '18 ноября 2015',
-        text: 'Ку!',
-        likesCount: 12,
-    },
-    {
-        id: 2,
-        name: 'Lex Luthor',
-        avatar: 'https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg',
-        date: '18 декабря 2015',
-        text: 'Как дела?',
-        likesCount: 10,
-    },
-]
-
-let messagesData: Array<MessageDataPropsType> = [
-    {
-        id: 1,
-        avatar: "https://shapka-youtube.ru/wp-content/uploads/2021/02/prikolnaya-avatarka-dlya-patsanov.jpg",
-        name: 'Ivanov Ivan',
-        time: '23:15',
-        text: 'React - JavaScript-библиотека для создания пользовательских интерфейсов',
-    },
-    {
-        id: 2,
-        avatar: "https://shapka-youtube.ru/wp-content/uploads/2021/05/prikolnaya-avatarka-na-telefon.jpg",
-        name: 'Petrov Dima',
-        time: '08:15',
-        text: 'Компоненты позволяют разбить интерфейс на независимые части, про которые легко думать в отдельности. Их можно складывать вместе и использовать несколько раз.',
-    },
-    {
-        id: 3,
-        avatar: "https://shapka-youtube.ru/wp-content/uploads/2021/01/funny-ava-7.jpg",
-        name: 'Semenov Sergey',
-        time: '12:15',
-        text: 'Это JSX — расширение языка JavaScript. Мы рекомендуем использовать его, когда требуется объяснить React, как должен выглядеть UI. JSX напоминает язык шаблонов, наделённый силой JavaScript.',
-    },
-]
 
 ReactDOM.render(
     <App
